3efa0b73e87f18a04f72f111f822c97e10fd4118
CallumDougan <practicalitymalfunction@gmail.com>
2015-11-27 10:59:38 +0000

    created report_capacity function

--- hotel_class.rb
+++ hotel_class.rb
@@ -1,3 +1,5 @@
+require_relative('room_class')
+
 class Hotel
 
 attr_reader :name, :single_rooms, :double_rooms
@@ -6,6 +8,13 @@ attr_reader :name, :single_rooms, :double_rooms
     @name = name
     @single_rooms = Array.new(singlerooms)
     @double_rooms = Array.new(doublerooms)
+    #not including all the rooms, instead filling each with single empty entry?
+  end
+
+  def report_capacity
+    total_rooms = [@single_rooms, @double_rooms]
+    occupied_rooms = total_rooms.flatten.compact    
+    puts total_rooms.length - occupied_rooms.length
   end
 
 end
--- hotel_runner.rb
+++ hotel_runner.rb
@@ -2,3 +2,5 @@ require_relative ('hotel_class')
 require_relative('room_class')
 # binding.pry;''
 hotel = Hotel.new("Barry",4,4)
+puts hotel.single_rooms
+hotel.report_capacity
